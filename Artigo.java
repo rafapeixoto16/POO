144a9fef8e3164dda51962f9ab4c8dc071e76cb6
Diogo <a94877@alunos.uminho.pt>
2023-04-14 18:32:39 +0100

    Classe Artigo

---
+++ Artigo.java
@@ -0,0 +1,135 @@
+import java.util.*;
+
+public class Artigo {
+    private boolean novo;
+    private int numDonos;
+    private int avaliacao;
+    private String descricao;
+    private String codigo;
+    private double precoBase;
+    private double correcaoPreco;
+
+    //Construtores
+
+    /**
+     * Construtor para um novo Artigo
+     */
+    public Artigo(boolean novo, String descricao, String codigo, double precoBase) {
+        this.novo = novo;
+        this.numDonos = 0;
+        this.avaliacao = 100;
+        this.descricao = descricao;
+        this.codigo = codigo;
+        this.precoBase = precoBase;
+        this.correcaoPreco = 0;
+    }
+
+    public Artigo (Artigo artigo){
+        this.novo = artigo.isNovo();
+        this.numDonos = artigo.getNumDonos();
+        this.avaliacao = artigo.getAvaliacao();
+        this.descricao = artigo.getDescricao();
+        this.codigo = artigo.getCodigo();
+        this.precoBase = artigo.getCorrecaoPreco();
+        this.correcaoPreco = artigo.getCorrecaoPreco();
+    }
+
+
+    //Getters
+    public boolean isNovo() {
+        return novo;
+    }
+    public int getNumDonos() {
+        return numDonos;
+    }
+
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public double getPrecoBase() {
+        return precoBase;
+    }
+
+    public double getCorrecaoPreco() {
+        return correcaoPreco;
+    }
+
+
+    //Setters
+    public void setNovo(boolean novo) {
+        this.novo = novo;
+    }
+
+    public void setNumDonos(int numDonos) {
+        this.numDonos = numDonos;
+    }
+
+    public void setAvaliacao(int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setPrecoBase(double precoBase) {
+        this.precoBase = precoBase;
+    }
+
+    public void setCorrecaoPreco(double correcaoPreco) {
+        this.correcaoPreco = correcaoPreco;
+    }
+
+    //equals
+    public boolean equals(Object o){
+        if (this == o) return true;
+
+        if ((o == null) || (this.getClass() != o.getClass())) return false;
+
+        Artigo umArtigo = (Artigo) o;
+        return (this.codigo.equals(umArtigo.getCodigo())); //possivelmente comparar os outros atributos
+    }
+
+    //Clone
+    @Override
+    public Artigo clone(){
+        return new Artigo(this);
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Estado do Artigo:" +
+                "\n    Estado -> " + auxNovo(novo) +
+                "\n    Numero de Donos -> " + numDonos +
+                "\n    Avaliacao -> " + avaliacao + "%" +
+                "\n    Descricao -> " + descricao +
+                "\n    precoBase -> " + precoBase + "simbEur" +
+                "\n    correcaoPreco -> " + correcaoPreco +
+                '\n';
+    }
+    //funcao auxiliar para o toString
+    private String auxNovo(boolean novo) {
+        if (novo) return "Novo"; else return "Usado";
+    }
+
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(novo,numDonos,avaliacao,descricao,codigo,precoBase,correcaoPreco);
+    }
+
+}
---
+++ Sapatilha.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Sapatilha {
+    private int tamanho;
+    private boolean atacadores; //sapatilhas podem ter: atacadores ou atilhos
+
+    private Date data;
+    private String cor;
+
+    //Construtores
+    //Getters
+    //Setters
+
+
+
+}
