1c4455089cc1d40b24d000f969c051356a475cb0
Peixotao <rafaelp22um@gmail.com>
2023-04-30 22:41:12 +0100

    alteraçoes

--- Tests/utilizadorTeste.java
+++ Tests/utilizadorTeste.java
@@ -1,5 +1,36 @@
 package Tests;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import src.Utilizador;
+
 public class utilizadorTeste {
+    public utilizadorTeste(){}
+
+    @BeforeEach
+    public void setUp() {}
+
+    @AfterEach
+    public void tearDown() {}
+
+    @Test
+    public void testConstructor() {
+        Utilizador utilizador = new Utilizador();
+        assertTrue(utilizador!=null);
+        utilizador = new Utilizador("email","Peixoto","Avenida das Cruzes",309144557);
+        assertTrue(utilizador!=null);
+    }
+
+    @Test
+    public void testGetMail(){
+        Utilizador utilizador = new Utilizador("email","Peixoto","Avenida das Cruzes",309144557);
+        assertTrue(utilizador.getEmail().equals("email"));
+    }
+
+
+
+
 
 }
