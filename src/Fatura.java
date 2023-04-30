91dabdc515f38bd36f379d184605d1b33f4850f0
Peixotao <rafaelp22um@gmail.com>
2023-04-30 22:06:29 +0100

    alteraçoes

--- src/Fatura.java
+++ src/Fatura.java
@@ -3,6 +3,16 @@ package src;
 public class Fatura {
     private Encomenda encomenda;
     private double preco;
+    private Utilizador vendedor;
+    private Utilizador comprador;
 
+    public Fatura(){
+        this.encomenda = null;
+        this.preco = 0;
+    }
 
+    public Fatura(Fatura fatura){
+        this.encomenda = fatura.encomenda;
+        this.preco = fatura.preco;
+    }
 }
