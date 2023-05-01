8fa6e316a0e55cd8aaaac52d3a5e5865b2fecbf5
Peixotao <rafaelp22um@gmail.com>
2023-05-01 18:36:40 +0100

    alteraçoes

--- src/Vintage.java
+++ src/Vintage.java
@@ -7,13 +7,14 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Vintage implements Serializable {
-    private final ListaArtigos listaArtigos;
-    private final ListaUtilizadores listaUtilizadores;
-    private final ListaTransportadoras listaTransportadoras;
-    private final ListaEncomendas listaEncomendas;
-    private final ListaFaturas listaFaturas;
+    private ListaArtigos listaArtigos;
+    private ListaUtilizadores listaUtilizadores;
+    private ListaTransportadoras listaTransportadoras;
+    private ListaEncomendas listaEncomendas;
+    private ListaFaturas listaFaturas;
     private LocalDate dataSistema;
     private Utilizador userLigado;
+
     public Vintage(){
         listaArtigos = new ListaArtigos();
         listaUtilizadores = new ListaUtilizadores();
@@ -21,6 +22,15 @@ public class Vintage implements Serializable {
         listaEncomendas = new ListaEncomendas();
         listaFaturas = new ListaFaturas();
         userLigado = new Utilizador();
+    }
+
+    public Vintage(Vintage vintage){
+        this.listaUtilizadores = vintage.listaUtilizadores.clone();
+        this.listaArtigos = vintage.listaArtigos.clone();
+        this.listaTransportadoras = vintage.listaTransportadoras.clone();
+        this.listaEncomendas = vintage.listaEncomendas.clone();
+        this.listaFaturas = vintage.listaFaturas;//todo clone fatura
+        this.userLigado = userLigado.clone();
     }
 
     //todo funçoes de jeito
@@ -41,4 +51,61 @@ public class Vintage implements Serializable {
             catch (UtilizadorNaoExiste | NullPointerException exception ) {}
         }
     }
+
+    public void logout(){
+        userLigado = null;
+    }
+
+    public ListaArtigos getListaArtigos() {
+        return listaArtigos;
+    }
+
+    public ListaUtilizadores getListaUtilizadores() {
+        return listaUtilizadores;
+    }
+
+    public ListaTransportadoras getListaTransportadoras() {
+        return listaTransportadoras;
+    }
+
+    public ListaEncomendas getListaEncomendas() {
+        return listaEncomendas;
+    }
+
+    public ListaFaturas getListaFaturas() {
+        return listaFaturas;
+    }
+
+    public Utilizador getUserLigado() {
+        return userLigado;
+    }
+
+    public void setListaArtigos(ListaArtigos listaArtigos) {
+        this.listaArtigos = listaArtigos;
+    }
+
+    public void setListaUtilizadores(ListaUtilizadores listaUtilizadores) {
+        this.listaUtilizadores = listaUtilizadores;
+    }
+
+    public void setListaTransportadoras(ListaTransportadoras listaTransportadoras) {
+        this.listaTransportadoras = listaTransportadoras;
+    }
+
+    public void setListaEncomendas(ListaEncomendas listaEncomendas) {
+        this.listaEncomendas = listaEncomendas;
+    }
+
+    public void setListaFaturas(ListaFaturas listaFaturas) {
+        this.listaFaturas = listaFaturas;
+    }
+
+    public void setDataSistema(LocalDate dataSistema) {
+        this.dataSistema = dataSistema;
+    }
+
+    public void setUserLigado(Utilizador userLigado) {
+        this.userLigado = userLigado;
+    }
+
 }
