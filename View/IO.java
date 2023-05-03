175b4cf6445e88e0b1f0ca4613d2575fef438297
Diogo Ferreira <79595376+DiogoUM@users.noreply.github.com>
2023-05-03 22:37:46 +0100

    Input e IO

---
+++ View/IO.java
@@ -0,0 +1,9 @@
+package View;
+
+import static java.lang.System.out;
+
+public class IO {
+    public static void error(String error) {
+        System.out.println("\nError: " + error);
+    }
+}
--- View/Input.java
+++ View/Input.java
@@ -93,6 +93,185 @@ public class Input {
         }
         return b;
     }
-    //lalala
+
+    public static void getEnter() {
+        out.print("\nPressione" + " ENTER " + "para continuar...");
+        Input.getString();
+    }
+
+    public static int getOption(int max) {
+        int option;
+        boolean ok = false;
+
+        do {
+            out.print("Escolha uma opção: ");
+            option = Input.getInt();
+
+            if (option >= 0 && option <= max)
+                ok = true;
+            else
+                IO.error("Opção Inválida");
+        } while (!ok);
+
+        return option;
+    }
+
+    public static LocalDate getDate(String text) {
+        out.println(text);
+        LocalDate date = null;
+        boolean valid = false;
+        String format = "dd-mm-aaaa";
+        DateTimeFormatter f = DateTimeFormatter.ofPattern(format);
+
+        do {
+            try {
+                out.print("Data no formato " + format + " : ");
+                date = LocalDate.parse(Input.getString(), f);
+                if (date != null) {
+                    valid = true;
+                }
+            } catch (DateTimeParseException e) {
+                IO.error(e.getMessage());
+            }
+        } while (!valid);
+
+        return date;
+    }
+
+    public static String getEmail() {
+        out.print("Email: ");
+        String email = Input.getString();
+        return email;
+    }
+    public static String getName() {
+        out.print("Nome: ");
+        String name = Input.getString();
+        return name;
+    }
+
+    public static String getMorada() {
+        out.print("Morada: ");
+        String morada = Input.getString();
+        return morada;
+    }
+
+    public static String getNIF() {
+        out.print("NIF: ");
+        String nif = Input.getString();
+        return nif;
+    }
+
+    public static int getNumDonos() {
+        out.print("Número de donos que o Artigo já teve: ");
+        int numDonos = Input.getInt();
+        return numDonos;
+    }
+
+    public static int getAvaliacao() {
+        int avaliacao;
+        boolean ok = false;
+        do {
+            out.print("Avaliação do Artigo (valor inteiro entre 0 e 10): ");
+            avaliacao = Input.getInt();
+
+            if (avaliacao>=0 && avaliacao <=10)
+                ok = true;
+            else
+                IO.error("Valor para Avaliação Inválido");
+        }while (!ok);
+        return avaliacao;
+    }
+
+    public static String getDescricao() {
+        out.print("Descricao do Artigo: ");
+        String descricao = Input.getString();
+        return descricao;
+    }
+
+    public static double getPrecoBase() {
+        out.print("Preço Base do Artigo: ");
+        double pb = Input.getDouble();
+        return pb;
+    }
+
+    public static int getDim() {
+        out.print("Dimensao da Mala: ");
+        int dim = Input.getInt();
+        return dim;
+    }
+
+    public static String getMaterial() {
+        out.print("Material da Mala: ");
+        String material = Input.getString();
+        return material;
+    }
+
+    //Para classe Mala
+    public static LocalDate getAno_colecao() { //rever
+        out.print("Ano de colecao da mala: ");
+        int ano = Input.getInt();
+        LocalDate data = LocalDate.of(ano,1,1);
+        return data;
+    }
+
+    public static int getTamanhoSapatilha() {
+        out.print("Tamanho da Sapatilha: ");
+        int tamanho = Input.getInt();
+        return tamanho;
+    }
+
+    public static boolean getAtacadores() {
+        out.print("Atacadores = 1\n");
+        out.print("Atilhos = 2\n");
+        out.print("Opção: ");
+        int op = Input.getInt();
+        boolean at;
+        if(op == 1) at = true;
+        else at = false;
+        return at;
+    }
+
+    //Para classe Spatilha
+    public static LocalDate getData() { //rever
+        out.print("Ano de colecao da Sapatilha: ");
+        int ano = Input.getInt();
+        LocalDate data = LocalDate.of(ano,1,1);
+        return data;
+    }
+
+    public static String getCor() {
+        out.print("Cor da Spatilha: ");
+        String cor = Input.getString();
+        return cor;
+    }
+
+    public static int getTamanhoTShirt() {
+        out.print("XL <- 3\n");
+        out.print("L  <- 2\n");
+        out.print("M  <- 1\n");
+        out.print("S  <- 0\n");
+        out.print("Tamanho da TShirt: ");
+        int tamanho = Input.getInt();
+        return tamanho;
+    }
+
+    public static int getPadrao() {
+        out.print("Palmeiras <- 2\n");
+        out.print("Riscas    <- 1\n");
+        out.print("Liso      <- 0\n");
+        out.print("Padrão da TShirt: ");
+        int padrao = Input.getInt();
+        return padrao;
+    }
+
+    //get para o codTransportadora????????
+
+    public static int getImposto() {
+        out.print("Imposto: ");
+        int imposto = Input.getInt();
+        return imposto;
+    }
+
+    //diasEntrega?????????????????
 
 }
