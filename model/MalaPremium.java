2506df7fc816c1bd7c707c75d5d028880c38bbb4
Diogo Ferreira <79595376+DiogoUM@users.noreply.github.com>
2023-05-08 17:49:44 +0100

    Premium

--- model/MalaPremium.java
+++ model/MalaPremium.java
@@ -1,11 +1,15 @@
 package model;
 
+import java.time.LocalDate;
+
 public class MalaPremium extends Mala implements Premium {
     public double calculaPrecoFinal(){
-        double precoFinal = getPrecoBase();
+        double precoBase = getPrecoBase();
+        int anoAual = LocalDate.now().getYear();
+        int anoMala = this.getAno_colecao().getYear();
+        int tamanho = this.getDim();
+        double precoFinal = precoBase + precoBase*0.1*(anoAual-anoMala);
+        return precoFinal;
 
-
-
-        return 1;
     }
 }
--- model/SapatilhaPremium.java
+++ model/SapatilhaPremium.java
@@ -1,11 +1,14 @@
 package model;
 
+import java.time.LocalDate;
+
 public class SapatilhaPremium extends Sapatilha implements Premium {
     public double calculaPrecoFinal(){
-        double precoFinal = getPrecoBase();
-
-
-
-        return 1;
+        //aumenta 10% do valor a cada ano que passa
+        double precoBase = getPrecoBase();
+        int anoAual = LocalDate.now().getYear();
+        int anoSapatilha = this.getData().getYear();
+        double precoFinal = precoBase + precoBase*0.1*(anoAual-anoSapatilha);
+        return precoFinal;
     }
 }
