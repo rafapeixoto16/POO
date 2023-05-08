848bf0bf116d7dbd44e490888764928be18e8f30
Peixotao <rafaelp22um@gmail.com>
2023-05-08 17:02:29 +0100

    alteraçoes

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -1,7 +1,15 @@
 package Controller;
 
+import Exceptions.UtilizadorJaExistente;
 import Serializacao.Serializacao;
+import model.Artigo;
+import model.Mala;
+import model.Utilizador;
 import model.Vintage;
+
+import java.time.LocalDate;
+
+import static java.lang.System.out;
 
 public class Controller {
     private static final String ficheiroDados = "ficheiro.dat";
@@ -12,6 +20,13 @@ public class Controller {
         this.vintage = new Vintage();
      }
      public void run(){
+         this.vintage = ficheiro.carregar();
+         try {
+             this.vintage.getListaArtigos().addArtigo(new Mala(true,1,10,"Cavalinho",70,4,"couro", LocalDate.of(2023,1,1),"mail2"));
+             out.print(" "+ Artigo.getNumeroArtigos()+"\n");
+             this.ficheiro.guardar(this.vintage);
+         }catch (Exception ignored){
 
+         }
      }
 }
